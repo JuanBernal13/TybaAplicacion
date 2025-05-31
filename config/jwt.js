233b80ac293894c77bd8c781db396c9343e42521
juanbernal13 <ja.bernalg1@uniandes.edu.co>
2025-05-31 12:08:00 -0500

    [Update]

--- config/jwt.js
+++ config/jwt.js
@@ -1,4 +1,4 @@
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || 'tu_clave_secreta_super_segura_aqui_2024',
+  JWT_SECRET: process.env.JWT_SECRET || '123',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '24h'
 }; 
